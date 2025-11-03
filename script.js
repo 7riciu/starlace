8d0482c9ea288372d12a87074e2ecdc4c0f9d26f
Tepordei Patricia <tepordeipatricia2007@gmail.com>
2025-11-03 22:22:06 +0200

    Made the object to appear randomly on the screen

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function Found() {
     window.alert("Found the star!")
 }
+
+function randomPosition() {
+    const star = document.getElementById("star");
+
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
+
+    const starWidth = star.offsetWidth;
+    const starHeight = star.offsetHeight;
+
+    const maxHorizontal = screenWidth - starWidth;
+    const maxVertical = screenHeight - starHeight;
+
+    const randomHorizontal = Math.floor(Math.random() * maxHorizontal);
+    const randomVertical = Math.floor(Math.random() * maxVertical);
+
+    star.style.left = randomHorizontal + "px";
+    star.style.top = randomVertical + "px";
+}
+
+window.onload = randomPosition;
