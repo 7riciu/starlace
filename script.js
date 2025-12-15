76036cbb87876dc2d7d8b9e076a4420812d97e33
Tepordei Patricia <tepordeipatricia2007@gmail.com>
2025-12-15 23:54:43 +0200

    I made a local storage for the level completion variables :) yay

--- script.js
+++ script.js
@@ -1,4 +1,69 @@
+let counter = 0;
+
 document.addEventListener("DOMContentLoaded", () => {
+    if (localStorage.getItem("two_comp") === null) {
+        localStorage.setItem("two_comp", "false");
+    };
+    if (localStorage.getItem("three_comp") === null) {
+        localStorage.setItem("three_comp", "false");
+    };
+    if (localStorage.getItem("four_comp") === null) {
+        localStorage.setItem("four_comp", "false");
+    };
+    if (localStorage.getItem("five_comp") === null) {
+        localStorage.setItem("five_comp", "false");
+    };
+    if (localStorage.getItem("six_comp") === null) {
+        localStorage.setItem("six_comp", "false");
+    };
+    if (localStorage.getItem("seven_comp") === null) {
+        localStorage.setItem("seven_comp", "false");
+    };
+    if (localStorage.getItem("eight_comp") === null) {
+        localStorage.setItem("eight_comp", "false");
+    };
+    if (localStorage.getItem("nine_comp") === null) {
+        localStorage.setItem("nine_comp", "false");
+    };
+    if (localStorage.getItem("ten_comp") === null) {
+        localStorage.setItem("ten_comp", "false");
+    };
+    if (localStorage.getItem("eleven_comp") === null) {
+        localStorage.setItem("eleven_comp", "false");
+    };
+    if (localStorage.getItem("twelve_comp") === null) {
+        localStorage.setItem("twelve_comp", "false");
+    };
+    if (localStorage.getItem("thirteen_comp") === null) {
+        localStorage.setItem("thirteen_comp", "false");
+    };
+
+    let two_comp = localStorage.getItem("two_comp") === "true";
+    let three_comp = localStorage.getItem("three_comp") === "true";
+    let four_comp = localStorage.getItem("four_comp") === "true";
+    let five_comp = localStorage.getItem("five_comp") === "true";
+    let six_comp = localStorage.getItem("six_comp") === "true";
+    let seven_comp = localStorage.getItem("seven_comp") === "true";
+    let eight_comp = localStorage.getItem("eight_comp") === "true";
+    let nine_comp = localStorage.getItem("nine_comp") === "true";
+    let ten_comp = localStorage.getItem("ten_comp") === "true";
+    let eleven_comp = localStorage.getItem("eleven_comp") === "true";
+    let twelve_comp = localStorage.getItem("twelve_comp") === "true";
+    let thirteen_comp = localStorage.getItem("thirteen_comp") === "true";
+
+    let level_two = document.getElementById("level_two");
+    let level_three = document.getElementById("level_three");
+    let level_four = document.getElementById("level_four");
+    let level_five = document.getElementById("level_five");
+    let level_six = document.getElementById("level_six");
+    let level_seven = document.getElementById("level_seven");
+    let level_eight = document.getElementById("level_eight");
+    let level_nine = document.getElementById("level_nine");
+    let level_ten = document.getElementById("level_ten");
+    let level_eleven = document.getElementById("level_eleven");
+    let level_twelve = document.getElementById("level_twelve");
+    let level_thirteen = document.getElementById("level_thirteen");
+
     if (document.body.id === "level_two") {
         star_count_two();
     }
@@ -45,32 +110,6 @@ document.addEventListener("DOMContentLoaded", () => {
         star.style.left = `${x}%`;
         star.style.top = `${y}%`;
     });
-
-    let two_comp = false;
-    let three_comp = false;
-    let four_comp = false;
-    let five_comp = false;
-    let six_comp = false;
-    let seven_comp = false;
-    let eight_comp = false;
-    let nine_comp = false;
-    let ten_comp = false;
-    let eleven_comp = false;
-    let twelve_comp = false;
-    let thirteen_comp = false;
-
-    let level_two = document.getElementById("level_two");
-    let level_three = document.getElementById("level_three");
-    let level_four = document.getElementById("level_four");
-    let level_five = document.getElementById("level_five");
-    let level_six = document.getElementById("level_six");
-    let level_seven = document.getElementById("level_seven");
-    let level_eight = document.getElementById("level_eight");
-    let level_nine = document.getElementById("level_nine");
-    let level_ten = document.getElementById("level_ten");
-    let level_eleven = document.getElementById("level_eleven");
-    let level_twelve = document.getElementById("level_twelve");
-    let level_thirteen = document.getElementById("level_thirteen");
 
     level_three.addEventListener("click", (level) => {
         if (!two_comp) {
@@ -176,8 +215,6 @@ function randomPosition() {
         });
 }
 
-let counter = 0;
-
 function star_count_two() {
     let stars = document.querySelectorAll(".star");
 
@@ -190,7 +227,7 @@ function star_count_two() {
             console.log(counter)
             if (counter === 13) {
                 window.location.href = "level_two_end.html";
-                two_comp = true;
+                localStorage.setItem("two_comp", "true");
                 counter = 0;
             }
         });
@@ -209,7 +246,7 @@ function star_count_three() {
             console.log(counter)
             if (counter === 13) {
                 window.location.href = "level_three_end.html";
-                three_comp = true;
+                localStorage.setItem("three_comp", "true");
                 counter = 0;
             }
         });
@@ -228,7 +265,7 @@ function star_count_four() {
             console.log(counter)
             if (counter === 16) {
                 window.location.href = "level_four_end.html";
-                four_comp = true;
+                localStorage.setItem("four_comp", "true");
                 counter = 0;
             }
         });
@@ -247,7 +284,7 @@ function star_count_five() {
             console.log(counter)
             if (counter === 11) {
                 window.location.href = "level_five_end.html";
-                five_comp = true;
+                localStorage.setItem("five_comp", "true");
                 counter = 0;
             }
         });
@@ -266,7 +303,7 @@ function star_count_six() {
             console.log(counter)
             if (counter === 14) {
                 window.location.href = "level_six_end.html";
-                six_comp = true;
+                localStorage.setItem("six_comp", "true");
                 counter = 0;
             }
         });
@@ -285,7 +322,7 @@ function star_count_seven() {
             console.log(counter)
             if (counter === 7) {
                 window.location.href = "level_seven_end.html";
-                seven_comp = true;
+                localStorage.setItem("seven_comp", "true");
                 counter = 0;
             }
         });
@@ -304,7 +341,7 @@ function star_count_eight() {
             console.log(counter)
             if (counter === 6) {
                 window.location.href = "level_eight_end.html";
-                eight_comp = true;
+                localStorage.setItem("eight_comp", "true");
                 counter = 0;
             }
         });
@@ -323,7 +360,7 @@ function star_count_nine() {
             console.log(counter)
             if (counter === 10) {
                 window.location.href = "level_nine_end.html";
-                nine_comp = true;
+                localStorage.setItem("nine_comp", "true");
                 counter = 0;
             }
         });
@@ -342,7 +379,7 @@ function star_count_ten() {
             console.log(counter)
             if (counter === 8) {
                 window.location.href = "level_ten_end.html";
-                ten_comp = true;
+                localStorage.setItem("ten_comp", "true");
                 counter = 0;
             }
         });
@@ -361,7 +398,7 @@ function star_count_eleven() {
             console.log(counter)
             if (counter === 9) {
                 window.location.href = "level_eleven_end.html";
-                eleven_comp = true;
+                localStorage.setItem("eleven_comp", "true");
                 counter = 0;
             }
         });
@@ -380,7 +417,7 @@ function star_count_twelve() {
             console.log(counter)
             if (counter === 10) {
                 window.location.href = "level_twelve_end.html";
-                twelve_comp = true;
+                localStorage.setItem("twelve_comp", "true");
                 counter = 0;
             }
         });
@@ -399,7 +436,7 @@ function star_count_thirteen() {
             console.log(counter)
             if (counter === 17) {
                 window.location.href = "win.html";
-                thirteen_comp = true;
+                localStorage.setItem("thirteen_comp", "true");
                 counter = 0;
             }
         });
